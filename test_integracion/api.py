02e7874d4ec878516f138ec67945ad8501bb00c6
Diego Duncan <diegoduncan21@gmail.com>
2014-08-08 23:57:33 -0300

    adding methods_allowed

--- test_integracion/api.py
+++ test_integracion/api.py
@@ -1,8 +1,13 @@
 from tastypie.resources import ModelResource
 from .models import Person
+from tastypie.authorization import Authorization
+from tastypie.authentication import Authentication
 
 
 class PersonResource(ModelResource):
     class Meta:
         queryset = Person.objects.all()
         resource_name = 'person'
+        allowed_methods = ['get', 'post', 'put', 'delete', 'patch']
+        authentication = Authentication()
+        authorization = Authorization()
