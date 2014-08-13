26eb25253c5fcdcdf03e69870e66f211b812c711
Diego Duncan <diegoduncan21@gmail.com>
2014-08-12 22:23:57 -0300

    arreglando la estructura de directorios

--- juego/bases/menus.py
+++ juego/bases/menus.py
@@ -1,11 +1,14 @@
 import pilas
 
+
 class MenuDeIconos(pilas.actores.Menu):
-	
-	def __init__(self, opciones, x=0, y=0, fuente=None,
-					color_normal=pilas.colores.gris,
-					color_resaltado=pilas.colores.blanco):
-						
-		op = [(icono[0], "", [icono[1]]) for icono in opciones]
-		pilas.actores.Menu.__init__(self, op, x=x, y=y, fuente=fuente,
-					color_normal=color_normal, color_resaltado=color_resaltado) 
+
+    def __init__(self, opciones, x=0, y=0, fuente=None,
+                 color_normal=pilas.colores.gris,
+                 color_resaltado=pilas.colores.blanco):
+
+        op = [(icono[0], "", [icono[1]]) for icono in opciones]
+        pilas.actores.Menu.__init__(self, op, x=x, y=y,
+                                    fuente=fuente,
+                                    color_normal=color_normal,
+                                    color_resaltado=color_resaltado)
--- juego/escenas/presentacion.py
+++ juego/escenas/presentacion.py
@@ -1,31 +1,33 @@
 import pilas
 from escenas.niveles.tutorial import EscenaPrimerDesafio
 from bases.menus import MenuDeIconos
+
+
 class EscenaDeMenu(pilas.escena.Base):
 
-	def __init__(self):
-		pilas.escena.Base.__init__(self)
+    def __init__(self):
+        pilas.escena.Base.__init__(self)
 
-	def iniciar(self):
-		pilas.fondos.Selva()
-		"""
-		opciones = [
-			('imagenes/menus/principal/jugar.png', "", [self.primer_desafio]),
-			('imagenes/menus/principal/salir.png', "", [self.salir])
-		]
-		"""
-		
-		opciones = [
-			('imagenes/menus/principal/jugar.png', self.primer_desafio), 
-			('imagenes/menus/principal/salir.png', self.salir), 
-		]
-		
-		#self.menu = pilas.actores.Menu(opciones)
-		self.menu = MenuDeIconos(opciones)
+    def iniciar(self):
+        pilas.fondos.Selva()
+        """
+        opciones = [
+            ('imagenes/menus/principal/jugar.png', "", [self.primer_desafio]),
+            ('imagenes/menus/principal/salir.png', "", [self.salir])
+        ]
+        """
 
-	def primer_desafio(self):
-		pilas.cambiar_escena(EscenaPrimerDesafio())
+        opciones = [
+            ('imagenes/menus/principal/jugar.png', self.primer_desafio),
+            ('imagenes/menus/principal/salir.png', self.salir),
+        ]
 
-	def salir(self):
-		import sys
-		sys.exit(0)
+        #self.menu = pilas.actores.Menu(opciones)
+        self.menu = MenuDeIconos(opciones)
+
+    def primer_desafio(self):
+        pilas.cambiar_escena(EscenaPrimerDesafio())
+
+    def salir(self):
+        import sys
+        sys.exit(0)
